6fbcdc73abf293b88dba99bf85dcdc15392823b0
Valts Vilcans <valts03@inbox.lv>
2020-10-01 10:22:15 +0200

    get data from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [coins, setCoins] = useState([]);
+  const [erroMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setErrorMsg(err);
+      });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
